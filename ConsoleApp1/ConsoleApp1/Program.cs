7ecf53e77a7c4892add1f757bb79287b379c2336
Hanneoul <elflee77@gmail.com>
2023-05-30 11:48:53 +0900

    elflee, 13강

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Task.Run(() => DoPrint()); // 1, 2, 3, ...
+            Console.WriteLine("[?] async await 사용 예제");
+            Thread.Sleep(1);
+        }
+        static async void DoPrint()
+        {
+            await PrintNumberAsync();
+        }
+        static async Task PrintNumberAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 300; i++)
+                {
+                    Console.WriteLine(i + 1);
+                }
+            });
+        }
+    }
+}
---
+++ lecture12/lecture12/main.c
@@ -0,0 +1,71 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+
+
+int frame0()
+{
+	printf("loading : #");
+	return 0;
+}
+
+int frame1()
+{
+	printf("loading : ####");
+	return 0;
+}
+
+int frame2()
+{
+	printf("loading : ########");
+	return 0;
+}
+
+int frame3()
+{
+	printf("loading : #############");
+	return 0;
+}
+
+int frame4()
+{
+	printf("Complete");
+	return 0;
+}
+
+
+
+int main()
+{
+	clock_t deltaTimeStart = clock(NULL);
+	
+	int i = 0;
+	
+	while (i < 1)
+	{
+		COORD pos = { 0,0 };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+
+		frame0();
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+		Sleep(1000);
+		frame1();
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+		Sleep(1000);
+		frame2();
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+		Sleep(1000);
+		frame3();
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+		Sleep(1000);
+		frame4();
+		i = i + 1;
+	}
+	clock_t deltaTimeEnd = clock(NULL);
+
+	printf("\n걸린 시간 : %d", deltaTimeEnd - deltaTimeStart);
+
+	return 0;
+}
